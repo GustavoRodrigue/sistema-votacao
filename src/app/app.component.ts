ab303ccd6f69b1b4fc6b9b83128aa20e3073a795
GustavoRodrigue <gustavorodriguesgra13@gmail.com>
2024-08-30 10:02:46 -0300

    cpmponente nav criado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
+  imports: [RouterOutlet,
+    MatButtonModule
+  ],
+  templateUrl:'app.component.html' ,
   styles: [],
 })
 export class AppComponent {
